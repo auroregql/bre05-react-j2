dbdb0c5b1f6794e159ea2a1fa29f40c02e66adb6
auroregql <aurore.gicquel.06@gmail.com>
2026-01-28 10:20:22 +0000

    Exerice 3 terminé : Rajout de mailto pour un lien dynamique

--- exercice3/src/user.jsx
+++ exercice3/src/user.jsx
@@ -2,7 +2,7 @@ const User =  (props) => {
     return (
     <article>
     <h2>Prénom et Nom de l'utilisateur {props.identity.firstName} {props.identity.lastName}</h2>
-    <a href="auroregql@gmail.com">Email de l'utilisateur {props.identity.email}</a>
+    <a href="mailto:auroregql@gmail.com">Email de l'utilisateur {props.identity.email}</a>
     </article>
     )
 }
