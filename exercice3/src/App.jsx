19603b8fbb6816892ea74f8357993155d69e2a90
auroregql <aurore.gicquel.06@gmail.com>
2026-01-28 10:01:34 +0000

    Exercice 3 terminé : Envoyer les props dans un objet

---
+++ exercice3/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import User from "./user.jsx";
+
+function App() {
+
+  return (
+   <User identity={{firstName:"Aurore", lastName:"GICQUEL", email:"auroregql@gmail.com"}}/>
+  )
+}
+
+export default App;
