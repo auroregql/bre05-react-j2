8c66441dbec3a2b8545523d70fbaf0b558153fed
auroregql <aurore.gicquel.06@gmail.com>
2026-01-28 16:09:34 +0000

    Exercice 2 terminé : state avec plusieurs informations

--- state/exercice2/src/user.jsx
+++ state/exercice2/src/user.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 
 const User = () => {
-  const [status, setStatus] = useState(false);
-  const [role, setRole] = useState("Anonyme");
+  const [userState, setUserState] = useState({
+    status: false,
+    role: "Anonyme",
+  });
 
   let statusMessage;
 
-  if (status === true) {
+  if (userState.status === true) {
     statusMessage = "En Ligne";
   } else {
     statusMessage = "Hors Ligne";
@@ -15,7 +17,7 @@ const User = () => {
   return (
     <article>
       <h2>Mari Doucet</h2>
-      <h3>{role}</h3>
+      <h3>{userState.role}</h3>
       <p>{statusMessage}</p>
     </article>
   );
